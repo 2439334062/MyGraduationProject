5fda6139f3990d596783c98a1509a2a21fcdacdc
2439334062 <2439334062@qq.com>
2020-12-20 01:14:12 +0800

    add README.txt......

---
+++ MyWeb_Hub/BLL/ChatDataBLL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using MyWeb_Hub.DAL;
+using MyWeb_Hub.Models;
+
+namespace MyWeb_Hub.BLL
+{
+    public class ChatDataBLL
+    {
+        /// <summary>
+        /// 把聊天数据写入数据库
+        /// </summary>
+        /// <param name="chatDataModels"></param>
+        /// <returns></returns>
+        public int AddChatDataBLL(ChatDataModels chatDataModels)
+        {
+            int a = new ChatDataDLL().AddChatDataDAL(chatDataModels);
+            if (a > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public DataTable GetFriendsDataBLL(string sender, string receiver)
+        {
+            DataSet ds = new ChatDataDLL().QueryChatDataDAL(sender,receiver);
+            DataTable dt = ds.Tables[0];
+            int len = dt.Rows.Count;
+
+            return dt;
+        }
+    }
+}
---
+++ MyWeb_Hub/DAL/GroupChatDAL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace MyWeb_Hub.DAL
+{
+    public class GroupChatDAL
+    {
+        public int AddRoomDAL(string roomName, string UserName)
+        {
+            if (GetOnly(roomName,UserName ))
+            {
+                string sql1 = "insert into [Chat].[dbo].[GroupChat] ([roomName],[userName],[Time]) values('{0}','{1}','{2}')";
+                sql1 = string.Format(sql1, roomName, UserName, DateTime.Now.ToString());
+                int a = SqlHelper.GetNonQuery(sql1);
+                if (a > 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 3;
+            }
+        }
+        public bool GetOnly(string roomName, string UserName)
+        {
+            string sql = "SELECT *  FROM [Chat].[dbo].[GroupChat]   where roomName='" + roomName + "' and userName ='"+UserName+"'";
+            DataSet ds = SqlHelper.GetDataSet(sql);
+            DataTable dt = ds.Tables[0];
+            int n = dt.Rows.Count;
+            if (n > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
